15ea60af83d31d6672738e98a57dacca9b102cf8
martxelo <marcelo.moreno.suarez@gmail.com>
2026-09-18 23:31:11 +0200

    Added hyprland config

---
+++ hypr/config/misc.lua
@@ -0,0 +1,31 @@
+hl.config({
+    dwindle = {
+        preserve_split = true,
+    },
+    general = {
+  	    layout = "scrolling",
+    },
+    scrolling = {
+        wrap_focus = true,
+    },
+    ecosystem = {
+        no_update_news = true,
+        no_donation_nag = true,
+    },
+    misc = {
+        col = {
+            splash = CACHYLGREEN,
+        },
+        middle_click_paste = false,
+        enable_swallow = true,
+        swallow_regex = "(kitty|ghostty|[Kk]onsole|Alacritty|gnome-terminal|xfce[0-9]?-terminal)",
+        vrr = 3,
+	on_focus_under_fullscreen = 2,
+    },
+    render = {
+        direct_scanout = 2,
+    },
+    xwayland = {
+        force_zero_scaling = true
+    },
+})
