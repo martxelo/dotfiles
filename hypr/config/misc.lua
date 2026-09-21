8eaaad8098895d13f98d35ecbae5f32fbe0aca0b
martxelo <marcelo.moreno.suarez@gmail.com>
2026-09-21 18:07:00 +0200

    Focus on mouse deactivated

--- hypr/config/misc.lua
+++ hypr/config/misc.lua
@@ -12,6 +12,9 @@ hl.config({
         no_update_news = true,
         no_donation_nag = true,
     },
+    input = {
+        follow_mouse = 0,
+    },
     misc = {
         col = {
             splash = CACHYLGREEN,
@@ -20,7 +23,7 @@ hl.config({
         enable_swallow = true,
         swallow_regex = "(kitty|ghostty|[Kk]onsole|Alacritty|gnome-terminal|xfce[0-9]?-terminal)",
         vrr = 3,
-	on_focus_under_fullscreen = 2,
+	      on_focus_under_fullscreen = 2,
     },
     render = {
         direct_scanout = 2,
